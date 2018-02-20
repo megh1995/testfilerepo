9ca89b7183a76a1540b487b11b1a7f30b7ec414b
EC2 Default User <ec2-user@ip-172-31-11-152.us-east-2.compute.internal>
2018-02-20 17:52:29 +0000

    the project

---
+++ 1struby.rb
@@ -0,0 +1,8 @@
+a = 10
+b = 3 * a + 2
+printf("%d %d\n", a, b);
+
+# Type is dynamic.
+b = "A string"
+c = 'Another String'
+print b + " and " + c + "\n"
---
+++ 3rdruby.rb
@@ -0,0 +1,2 @@
+File.new("simple_file.txt", "w+")
+File.delete("simple_file.txt")
